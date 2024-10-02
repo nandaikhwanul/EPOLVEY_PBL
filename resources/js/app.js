6172bbb1e53ba14ae8102da4969179917c8cc8b6
AndikaJayaNanda <andikajn25@gmail.com>
2024-10-02 16:53:29 +0700

    Fetch

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Example from './components/Example';
+
+ReactDOM.createRoot(document.getElementById('app')).render(<Example />);
